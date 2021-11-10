e07e4dcae022ec977d9a9ec3984271ce7663fa37
schelip <fcominscheffel@gmail.com>
2021-11-10 10:47:46 -0300

    Adicionada classe helpers com função de utilidade para injeção de dependências usando reflexão

---
+++ ApiCatalogoJogos/Extensions/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ApiCatalogoJogos.Extensions
+{
+    public static class Helpers
+    {
+        public static IEnumerable<Type> GetTypes(string nameSpace, Assembly assembly)
+        {
+            return assembly.GetTypes().Where(t => t.Namespace == nameSpace);
+        }
+    }
+}
