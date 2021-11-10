6c7a8069ef35eeb60e90aba14a822dc8df8fb874
schelip <fcominscheffel@gmail.com>
2021-11-10 10:44:52 -0300

    Maior abstração dos contratos de repositorios

--- ApiCatalogoJogos/Business/Repositories/IProdutoraRepository.cs
+++ ApiCatalogoJogos/Business/Repositories/IProdutoraRepository.cs
@@ -5,34 +5,8 @@ using ApiCatalogoJogos.Business.Entities;
 
 namespace ApiCatalogoJogos.Business.Repositories
 {
-    public interface IProdutoraRepository : IDisposable
+    public interface IProdutoraRepository : IRepositoryBase<Produtora>, IDisposable
     {
-        /// <summary>
-        /// Obtém uma determinada quantidade de produtoras do contexto a partir de um offset
-        /// </summary>
-        /// <param name="pagina">Offset (1 = nenhum offset)</param>
-        /// <param name="quantidade">Quantidade de produtoras por página</param>
-        /// <returns>Lista de produtoras obtidas</returns>
-        Task<List<Produtora>> Obter(int pagina, int quantidade);
-        /// <summary>
-        /// Obtém todas as produtoras de um país no contexto
-        /// </summary>
-        /// <param name="isoPais">Codigo ISO de duas letras do país de origem das produtoras a serem obtidas</param>
-        /// <returns>Lista das produtoras obtidas</returns>
-        Task<List<Produtora>> Obter(string isoPais);
-        /// <summary>
-        /// Obtém produtora do contexto a partir do nome e do país de origem.
-        /// </summary>
-        /// <param name="nome">Nome da produtora</param>
-        /// <param name="isoPais">ISO de duas letras do país de origem</param>
-        /// <returns>Produtora recuperada</returns>
-        Task<Produtora> Obter(string nome, string isoPais);
-        /// <summary>
-        /// Obtém produtora do contexto pelo seu Id
-        /// </summary>
-        /// <param name="guid">Id da produtora a ser obtida</param>
-        /// <returns>Dados da produtora obtida</returns>
-        Task<Produtora> Obter(Guid guid);
         /// <summary>
         /// Obtém produtoras filhas do contexto a partir do Id da produtora mãe
         /// </summary>
@@ -46,29 +20,11 @@ namespace ApiCatalogoJogos.Business.Repositories
         /// <returns>Lista de jogos obtidos</returns>
         Task<List<Jogo>> ObterJogos(Guid id);
         /// <summary>
-        /// Insere produtora no contexto
-        /// </summary>
-        /// <param name="produtora">Produtora a ser inserida</param>
-        /// <returns>Dados da produtora inserida</returns>
-        Task<Produtora> Inserir(Produtora produtora);
-        /// <summary>
-        /// Atualiza todos os dados de uma produtora no contexto
-        /// </summary>
-        /// <param name="produtora">Novos dados da produtora</param>
-        /// <returns>Dados atualizados da produtora atualizada</returns>
-        Task<Produtora> Atualizar(Produtora produtora);
-        /// <summary>
         /// Adicionar produtora filha a produtora no contexto
         /// </summary>
         /// <param name="mae">Id da produtora mãe</param>
         /// <param name="filha">Id da produtora filha</param>
         /// <returns>Dados atualizados da rodutora mãe</returns>
-        Task<Produtora> Atualizar(Produtora mae, Produtora filha);
-        /// <summary>
-        /// Remove produtora do contexto
-        /// </summary>
-        /// <param name="id">Id da produtora a ser removida</param>
-        /// <returns></returns>
-        Task Remover(Guid id);
+        Task<Produtora> AdicionarFilha(Produtora mae, Produtora filha);
     }
 }
---
+++ ApiCatalogoJogos/Business/Repositories/IRepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiCatalogoJogos.Business.Entities;
+
+namespace ApiCatalogoJogos.Business.Repositories
+{
+    public interface IRepositoryBase<T> where T : EntityBase
+    {
+        /// <summary>
+        /// Obtém uma determinada quantidade de entidades do contexto a partir de um offset
+        /// </summary>
+        /// <param name="pagina">Offset (1 = nenhum offset)</param>
+        /// <param name="quantidade">Quantidade de entidades por página</param>
+        /// <returns>Lista das entidades obtidas</returns>
+        Task<List<T>> Obter(int pagina, int quantidade);
+        /// <summary>
+        /// Obtém entidades que respeitam uma lista de parametros
+        /// </summary>
+        /// <param name="ps">Lista dos parametros que devem ser validados (nome, valor)</param>
+        /// <returns>Entidades obtidas</returns>
+        Task<List<T>> Obter(params (string, object)[] ps);
+        /// <summary>
+        /// Obtém entidades de um tipo possivelmente diferente de <c>T</c> que respeitam uma lista de parametros
+        /// </summary>
+        /// <typeparam name="TExternal">Tipo das entidade a serem obtidas</typeparam>
+        /// <param name="ps">Lista dos parametros que devem ser validados (nome, valor)</param>
+        /// <returns>Entidades obtidas</returns>
+        Task<List<TExternal>> Obter<TExternal>(params (string, object)[] ps) where TExternal : EntityBase;
+        /// <summary>
+        /// Obtém entidade do contexto pelo seu Id
+        /// </summary>
+        /// <param name="id">Id da entidade a ser obtida</param>
+        /// <returns>Entidade obtida</returns>
+        Task<T> Obter(Guid id);
+        /// <summary>
+        /// Insere entidade no contexto
+        /// </summary>
+        /// <param name="entidade">Entidade a ser inserida</param>
+        /// <returns>Entidade inserida</returns>
+        Task<T> Inserir(T entidade);
+        /// <summary>
+        /// Atualiza todos os dados de uma produtora no contexto
+        /// </summary>
+        /// <param name="entidade">Novos dados da produtora</param>
+        /// <returns>Entidade atualizada</returns>
+        Task<T> Atualizar(T entidade);
+        /// <summary>
+        /// Remove entidade do contexto
+        /// </summary>
+        /// <param name="id">Id da entidade a ser removida</param>
+        /// <returns></returns>
+        Task Remover(Guid id);
+        void Dispose();
+    }
+}
