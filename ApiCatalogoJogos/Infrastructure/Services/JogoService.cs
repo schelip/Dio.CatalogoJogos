47c423471d9dd41f32ac09719ba32bfde524a4a4
schelip <fcominscheffel@gmail.com>
2021-11-10 10:46:46 -0300

    Maior abstração dos serviços

--- ApiCatalogoJogos/Infrastructure/Services/JogoService.cs
+++ ApiCatalogoJogos/Infrastructure/Services/JogoService.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using ApiCatalogoJogos.Business.Entities;
-using ApiCatalogoJogos.Business.Exceptions;
 using ApiCatalogoJogos.Business.Repositories;
 using ApiCatalogoJogos.Business.Services;
 using ApiCatalogoJogos.Infrastructure.Model.InputModel;
@@ -11,109 +8,45 @@ using ApiCatalogoJogos.Infrastructure.Model.ViewModel;
 
 namespace ApiCatalogoJogos.Infrastructure.Services
 {
-    public class JogoService : IJogoService
+    public class JogoService : ServiceBase<JogoInputModel, JogoViewModel, Jogo>, IJogoService
     {
-        private readonly IJogoRepository _repository;
-
-        public JogoService(IJogoRepository repository)
+        public JogoService(IJogoRepository repository) : base(repository)
         {
-            _repository = repository;
         }
 
-        public async Task<List<JogoViewModel>> Obter(int pagina, int quantidade)
+        protected override async Task<Jogo> ObterEntidade(Guid guid, JogoInputModel inputModel)
         {
-            var jogos = await _repository.Obter(pagina, quantidade);
+            var jogo = guid == Guid.Empty
+                               ? new Jogo() { Id = Guid.NewGuid() }
+                               : await _repository.Obter(guid);
 
-            return await ObterViewModels(jogos);
+            jogo.Nome = inputModel.Nome;
+            jogo.Ano = inputModel.Ano;
+            jogo.ProdutoraId = inputModel.ProdutoraId;
+
+            return jogo;
         }
 
-        public async Task<JogoViewModel> Obter(Guid id)
+        protected override Task<JogoViewModel> ObterViewModel(Jogo jogo)
         {
-            var jogo = await _repository.Obter(id);
-
-            if (jogo == null)
-                throw new EntidadeNaoCadastradaException("Jogo não cadastrado");
-
-            return await ObterViewModel(jogo);
-        }
-
-        public async Task<JogoViewModel> Inserir(JogoInputModel jogoInput)
-        {
-            var jogos = await _repository.Obter(jogoInput.Nome, jogoInput.ProdutoraId);
-
-            if (jogos.Count > 0)
-            {
-                var ex = new EntidadeJaCadastradaException("Jogo com mesmo nome já cadastrado para esta produtora");
-                ex.Data.Add("JogoConflitante", jogos.First());
-                throw ex;
-            }
-
-            var jogo = new Jogo()
-            {
-                Id = Guid.NewGuid(),
-                Nome = jogoInput.Nome,
-                ProdutoraId = jogoInput.ProdutoraId,
-                Ano = jogoInput.Ano
-            };
-
-            await _repository.Inserir(jogo);
-
-            return await ObterViewModel(jogo);
-        }
-
-        public async Task<JogoViewModel> Atualizar(Guid id, JogoInputModel jogoInput)
-        {
-            var jogo = await _repository.Obter(id);
-
-            if (jogo == null)
-                throw new EntidadeNaoCadastradaException("Jogo não cadastrado");
-
-            jogo.Nome = jogoInput.Nome;
-            jogo.ProdutoraId = jogoInput.ProdutoraId;
-            jogo.Ano = jogoInput.Ano;
-
-            await _repository.Atualizar(jogo);
-
-            return await ObterViewModel(jogo);
-        }
-
-        public async Task Remover(Guid id)
-        {
-            var jogo = await _repository.Obter(id);
-
-            if (jogo == null)
-                throw new EntidadeNaoCadastradaException("Jogo não cadastrado");
-
-            await _repository.Remover(id);
-        }
-
-        public void Dispose()
-        {
-            _repository?.Dispose();
-        }
-
-        // Util
-        private async Task<JogoViewModel> ObterViewModel(Jogo jogo)
-        {
-            return new JogoViewModel()
+            return Task.FromResult(new JogoViewModel()
             {
                 Id = jogo.Id,
                 Nome = jogo.Nome,
-                ProdutoraId = jogo.ProdutoraId,
-                Ano = jogo.Ano
-            };
+                Ano = jogo.Ano,
+                ProdutoraId = jogo.ProdutoraId
+            });
         }
 
-        private async Task<List<JogoViewModel>> ObterViewModels(List<Jogo> jogos)
+        protected override (string, object)[] ObterParametrosParaConflito(Jogo jogo)
         {
-            var list = new List<JogoViewModel>();
-
-            foreach (var jogo in jogos)
+            var prms = new (string, object)[]
             {
-                list.Add(await ObterViewModel(jogo));
-            }
+                ("Nome", jogo.Nome),
+                ("ProdutoraId", jogo.ProdutoraId)
+            };
 
-            return list;
+            return prms;
         }
     }
 }
--- ApiCatalogoJogos/Infrastructure/Services/ProdutoraService.cs
+++ ApiCatalogoJogos/Infrastructure/Services/ProdutoraService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiCatalogoJogos.Business.Entities;
@@ -12,132 +10,35 @@ using ApiCatalogoJogos.Infrastructure.Model.ViewModel;
 
 namespace ApiCatalogoJogos.Infrastructure.Services
 {
-    public class ProdutoraService : IProdutoraService
+    public class ProdutoraService : ServiceBase<ProdutoraInputModel, ProdutoraViewModel, Produtora>, IProdutoraService
     {
-        private readonly IProdutoraRepository _repository;
-
-        public ProdutoraService(IProdutoraRepository repository)
+        public ProdutoraService(IProdutoraRepository repository) : base(repository)
         {
-            _repository = repository;
         }
 
-        public async Task<List<ProdutoraViewModel>> Obter(int pagina, int quantidade)
+        protected override async Task<Produtora> ObterEntidade(Guid guid, ProdutoraInputModel inputModel)
         {
-            var produtoras = await _repository.Obter(pagina, quantidade);
+            var produtora = guid == Guid.Empty
+                                            ? new Produtora() { Id = Guid.NewGuid() }
+                                            : await _repository.Obter(guid);
 
-            return await ObterViewModels(produtoras);
+            produtora.Nome = inputModel.Nome;
+            produtora.ISOPais = inputModel.ISOPais.ToUpper();
+            produtora.ProdutoraMae = inputModel.ProdutoraMaeId.HasValue
+                                    ? await _repository.Obter(inputModel.ProdutoraMaeId.Value)
+                                            ?? throw new EntidadeNaoCadastradaException()
+                                    : null;
+
+            return produtora;
         }
 
-        public async Task<List<ProdutoraViewModel>> Obter(string isoPais)
+        protected override async Task<ProdutoraViewModel> ObterViewModel(Produtora produtora)
         {
-            if (!ValidaPais(isoPais))
-                throw new PaisInexistenteException("País não encontrado");
 
-            var produtoras = await _repository.Obter(isoPais);
-
-            return await ObterViewModels(produtoras);
-        }
-
-        public async Task<ProdutoraViewModel> Obter(Guid id)
-        {
-            var produtora = await _repository.Obter(id);
-
-            if (produtora == null)
-                throw new EntidadeNaoCadastradaException("Produtora não cadastrada");
-
-            return await ObterViewModel(produtora);
-        }
-
-        public async Task<ProdutoraViewModel> Inserir(ProdutoraInputModel produtoraInput)
-        {
-            if (!ValidaPais(produtoraInput.ISOPais))
-                throw new PaisInexistenteException("País não encontrado");
-
-            var produtoraConflitante = await _repository.Obter(produtoraInput.Nome, produtoraInput.ISOPais);
-
-            if (produtoraConflitante != null)
-            {
-                var ex = new EntidadeJaCadastradaException("Produtora com mesmo nome e país de origem já cadastrada");
-                ex.Data.Add("IdProdutoraConflitante", produtoraConflitante.Id);
-                throw ex;
-            }
-
-            var produtora = new Produtora()
-            {
-                Id = Guid.NewGuid(),
-                Nome = produtoraInput.Nome,
-                ISOPais = produtoraInput.ISOPais,
-                ProdutoraMae = produtoraInput.ProdutoraMaeId.HasValue
-                    ? await _repository.Obter(produtoraInput.ProdutoraMaeId.Value)
-                    ?? throw new EntidadeNaoCadastradaException("Produtora mãe com id informado não encontrada")
-                    : null
-            };
-
-            await _repository.Inserir(produtora);
-
-            return await ObterViewModel(produtora);
-        }
-
-        public async Task<ProdutoraViewModel> Atualizar(Guid id, ProdutoraInputModel produtoraInput)
-        {
-            var produtora = await _repository.Obter(id);
-
-            if (produtora == null)
-                throw new EntidadeNaoCadastradaException("Entidade não cadastrada");
-
-            if (!ValidaPais(produtoraInput.ISOPais))
-                throw new PaisInexistenteException("País não encontrado");
-
-            produtora.Nome = produtoraInput.Nome;
-            produtora.ISOPais = produtoraInput.ISOPais;
-            produtora.ProdutoraMae = produtoraInput.ProdutoraMaeId.HasValue
-                ? await _repository.Obter(produtoraInput.ProdutoraMaeId.Value)
-                ?? throw new EntidadeNaoCadastradaException()
-                : null;
-
-            await _repository.Atualizar(produtora);
-
-            return await ObterViewModel(produtora);
-        }
-
-        public async Task<ProdutoraViewModel> Atualizar(Guid idMae, Guid idFilha)
-        {
-            var mae = await _repository.Obter(idMae);
-            var filha = await _repository.Obter(idFilha);
-
-            if (mae == null)
-                throw new EntidadeNaoCadastradaException("Produtora mãe não cadastrada");
-
-            if (filha == null)
-                throw new EntidadeNaoCadastradaException("Produtora filha não cadastrada");
-
-            if ((await _repository.ObterFilhas(mae.Id)).Contains(filha))
-                throw new EntidadeJaCadastradaException("Produtoras já estão cadastradas como mãe e filha");
-
-            var produtora = await _repository.Atualizar(mae, filha);
-
-            return await ObterViewModel(produtora);
-        }
-
-        public async Task Remover(Guid id)
-        {
-            var produtora = await _repository.Obter(id);
-
-            if (produtora == null)
-                throw new EntidadeNaoCadastradaException("Produtora não cadastrada");
-
-            await _repository.Remover(id);
-        }
-
-        public void Dispose()
-        {
-            _repository?.Dispose();
-        }
-
-        // Util
-        private async Task<ProdutoraViewModel> ObterViewModel(Produtora produtora)
-        {
-            var filhas = await _repository.ObterFilhas(produtora.Id);
+            var filhas = await _repository.Obter(("ProdutoraMae", produtora));
+            var jogos = await _repository.Obter<Jogo>(("Produtora", produtora));
+            foreach (var filha in filhas)
+                jogos.AddRange(await _repository.Obter<Jogo>(("Produtora", filha)));
 
             return new ProdutoraViewModel()
             {
@@ -145,46 +46,22 @@ namespace ApiCatalogoJogos.Infrastructure.Services
                 Nome = produtora.Nome,
                 ISOPais = produtora.ISOPais,
                 ProdutoraMaeId = produtora.ProdutoraMae == null ? Guid.Empty : produtora.ProdutoraMae.Id,
-                ProdutorasFilhas = filhas.Select(f => f.Id).ToList(),
-                JogosProduzidos = (await _repository.ObterJogos(produtora.Id)).Select(j => j.Id).ToList()
+                ProdutorasFilhas = filhas.Select(p => p.Id).ToList(),
+                JogosProduzidos = jogos.Select(j => j.Id).ToList()
             };
         }
 
-        private async Task<List<ProdutoraViewModel>> ObterViewModels(List<Produtora> produtoras)
+        protected override (string, object)[] ObterParametrosParaConflito(Produtora produtora)
         {
-            List<ProdutoraViewModel> list = new List<ProdutoraViewModel>();
-
-            foreach (var produtora in produtoras)
+            var ps = new (string, object)[]
             {
-                list.Add(await ObterViewModel(produtora));
-            }
+                ("Nome", produtora.Nome)
+            };
 
-            return list;
-        }
+            if (produtora.ProdutoraMae != null)
+                ps.Append(("ProdutoraMae", produtora.ProdutoraMae));
 
-        private List<JogoViewModel> ObterViewModels(List<Jogo> jogos)
-        {
-            List<JogoViewModel> list = new List<JogoViewModel>();
-
-            foreach (var jogo in jogos)
-            {
-                list.Add(new JogoViewModel()
-                {
-                    Id = jogo.Id,
-                    Nome = jogo.Nome,
-                    ProdutoraId = jogo.ProdutoraId,
-                    Ano = jogo.Ano
-                });
-            }
-
-            return list;
-        }
-
-        private static bool ValidaPais(string isoPais)
-        {
-            return CultureInfo.GetCultures(CultureTypes.SpecificCultures)
-                .Select(ci => new RegionInfo(ci.LCID))
-                .Any(ri => ri.TwoLetterISORegionName == isoPais.ToUpper());
+            return ps;
         }
     }
 }
