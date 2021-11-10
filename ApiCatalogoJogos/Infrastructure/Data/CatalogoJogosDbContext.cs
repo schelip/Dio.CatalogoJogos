4d71801cec2421197c0cab56b21586c6d6d98fac
schelip <fcominscheffel@gmail.com>
2021-11-10 10:46:32 -0300

    Adicionada função de utilidade usada para aumentar abstração dos repositorios

--- ApiCatalogoJogos/Infrastructure/Data/CatalogoJogosDbContext.cs
+++ ApiCatalogoJogos/Infrastructure/Data/CatalogoJogosDbContext.cs
@@ -11,15 +11,25 @@ namespace ApiCatalogoJogos.Data.Infrastructure
 
         public CatalogoJogosDbContext(DbContextOptions<CatalogoJogosDbContext> options)
             : base(options)
-        {
-
-        }
+        {}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new JogoMapping());
             modelBuilder.ApplyConfiguration(new ProdutoraMapping());
             base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<T> GetDbSet<T>() where T: EntityBase
+        {
+            var properties = GetType().GetProperties();
+            var targetType = typeof(DbSet<>).MakeGenericType(typeof(T));
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == targetType)
+                    return (DbSet<T>)property.GetValue(this);
+            }
+            return null;
         }
     }
 }
