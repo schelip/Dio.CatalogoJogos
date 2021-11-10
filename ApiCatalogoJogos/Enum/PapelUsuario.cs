fc24339e491161f610adf722438f0bbf9a319265
schelip <fcominscheffel@gmail.com>
2021-11-10 17:52:46 -0300

    Adicionado Enum com níveis de permissão do usuário

---
+++ ApiCatalogoJogos/Enum/PapelUsuario.cs
@@ -0,0 +1,21 @@
+﻿namespace ApiCatalogoJogos.Enum
+{
+    /// <summary>
+    /// Enum dos níveis de permissão dos usuários.
+    /// </summary>
+    public enum PermissaoUsuario
+    {
+        /// <summary>
+        /// Permissao para visualizar classes (outros usuarios exclusos)
+        /// </summary>
+        Usuario = 0,
+        /// <summary>
+        /// Permissao para atualizar e remover usuarios
+        /// </summary>
+        Moderardor = 1,
+        /// <summary>
+        /// Permissao para visualizar, adicionar, atualizar e remover todas as entidades
+        /// </summary>
+        Administrador = 2
+    }
+}
