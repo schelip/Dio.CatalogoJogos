6bc0081f40a1bf6a2f783bf966610e3c24e85267
schelip <fcominscheffel@gmail.com>
2021-11-12 00:17:55 -0300

    Entidades, Exceções e Contratos atualizados

---
+++ ApiCatalogoJogos/Business/Exceptions/AutenticacaoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ApiCatalogoJogos.Business.Exceptions
+{
+
+    [Serializable]
+    public class AutenticacaoException : Exception
+    {
+        public AutenticacaoException() : base("Erro durante autenticação") { }
+        public AutenticacaoException(string message) : base(message) { }
+        public AutenticacaoException(string message, Exception inner) : base(message, inner) { }
+        protected AutenticacaoException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- ApiCatalogoJogos/Business/Services/IUsuarioService.cs
+++ ApiCatalogoJogos/Business/Services/IUsuarioService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using ApiCatalogoJogos.Business.Exceptions;
 using ApiCatalogoJogos.Infrastructure.Model.InputModel;
 using ApiCatalogoJogos.Infrastructure.Model.ViewModel;
 
@@ -13,6 +14,8 @@ namespace ApiCatalogoJogos.Business.Services
         /// <param name="idUsuario">Id do usuário</param>
         /// <param name="idJogo">Id do jogo</param>
         /// <returns>ViewModel atualizada do usuário</returns>
+        /// <exception cref="EntidadeNaoCadastradaException"/>
+        /// <exception cref="FundosInsuficientesException"/>
         Task<UsuarioViewModel> AdicionarJogo(Guid idUsuario, Guid idJogo);
         /// <summary>
         /// Atualiza os fundos de um usuário
@@ -20,6 +23,20 @@ namespace ApiCatalogoJogos.Business.Services
         /// <param name="guid">Id do usuário</param>
         /// <param name="quant">Quantidade de fundos</param>
         /// <returns>ViewModel atualizada do usuário</returns>
+        /// <exception cref="EntidadeNaoCadastradaException"/>
         Task<UsuarioViewModel> AtualizarFundos(Guid guid, float quant);
+        /// <summary>
+        /// Autentica usuario e retorna token jwt
+        /// </summary>
+        /// <param name="inputModel">LoginInputModel com os dados a serem validados</param>
+        /// <returns>ViewModel da autenticacao</returns>
+        Task<(string, UsuarioViewModel)> Autenticar(LoginInputModel inputModel);
+        /// <summary>
+        /// Obtém ViewModel de usuário a partir de seu email
+        /// </summary>
+        /// <param name="email">Email do usuário a ser obtido</param>
+        /// <returns>ViewModel do usuário encontrado obtida</returns>
+        /// <exception cref="EntidadeNaoCadastradaException"/>
+        Task<UsuarioViewModel> Obter(string email);
     }
 }
